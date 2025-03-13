c01d9e22be1cfcd27d71c56b9fe4dcc79952e28f
Gabriel de Souza da Silva <biell@MacBook-Air-de-Gabriel.local>
2025-03-13 16:31:59 -0300

    fix: Sobe código já feito

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -12,6 +12,8 @@ import FooterSocial from "../components/sections/FooterSocial";
 import FloatingWhatsappButton from "../components/interactives/FloatingWhatsappButton";
 import BackToTopButton from "../components/interactives/BackToTopButton";
 
+// comentario para subir e abrir pr
+
 export default function Index() {
   return (
     <div>
